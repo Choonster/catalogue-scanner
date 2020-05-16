f4a8612109e816b8ab7ccb599ff4d43e9265bf92
Choonster TheMage <Choonster.2010@gmail.com>
2020-05-17 00:17:11 +1000

    Implement full email template

--- catalogue-scanner/SendCatalogueDigestEmail/SendCatalogueDigestEmail.cs
+++ catalogue-scanner/SendCatalogueDigestEmail/SendCatalogueDigestEmail.cs
@@ -1,11 +1,16 @@
 using CatalogueScanner.Dto.Config;
 using CatalogueScanner.Dto.FunctionResult;
+using HtmlAgilityPack;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.DurableTask;
 using Microsoft.Extensions.Localization;
 using Microsoft.Extensions.Options;
+using SendGrid;
 using SendGrid.Helpers.Mail;
 using System;
+using System.IO;
+using System.Linq;
+using System.Text;
 
 namespace CatalogueScanner
 {
@@ -48,16 +53,16 @@ namespace CatalogueScanner
 
         [FunctionName("SendCatalogueDigestEmail")]
         [return: SendGrid]
-        public SendGridMessage Run([ActivityTrigger] Catalogue catalogue)
+        public SendGridMessage Run([ActivityTrigger] Catalogue filteredCatalogue)
         {
             #region null checks
-            if (catalogue is null)
+            if (filteredCatalogue is null)
             {
-                throw new ArgumentNullException(nameof(catalogue));
+                throw new ArgumentNullException(nameof(filteredCatalogue));
             }
             #endregion
 
-            var summary = S.Plural(catalogue.Items.Count, "Catalogue Scanner found 1 matching item at {1}", "Catalogue Scanner found {0} matching items at {1}", catalogue.Store);
+            var summary = S.Plural(filteredCatalogue.Items.Count, "Catalogue Scanner found 1 matching item at {1}", "Catalogue Scanner found {0} matching items at {1}", filteredCatalogue.Store);
 
             var message = new SendGridMessage()
             {
@@ -67,9 +72,120 @@ namespace CatalogueScanner
 
             message.AddTo(settings.ToEmail, settings.ToName);
 
-            message.AddContent("text/plain", summary);
+            message.AddContent(MimeType.Html, GetHtmlContent(summary, filteredCatalogue));
+            message.AddContent(MimeType.Text, GetPlainTextContent(summary, filteredCatalogue));
 
             return message;
         }
+
+        private string GetHtmlContent(string summary, Catalogue filteredCatalogue)
+        {
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(EmailTemplates.HtmlTemplate);
+
+            #region Title
+            var titleNode = htmlDocument.DocumentNode
+                        .Descendants("title")
+                        .First();
+
+            titleNode.AppendChild(htmlDocument.CreateTextNode(summary));
+            #endregion
+
+            #region Intro Text
+            var introTextNodes = htmlDocument.DocumentNode
+                        .Descendants()
+                        .Where(node => node.HasClass("intro-text"));
+
+            foreach (var node in introTextNodes)
+            {
+                node.AppendChild(htmlDocument.CreateTextNode(summary));
+            }
+            #endregion
+
+            #region Items table
+            var itemsTableContainerNode = htmlDocument.GetElementbyId("items-table-container");
+
+            var table = itemsTableContainerNode.AppendChild(htmlDocument.CreateElement("table"));
+
+            #region Table header
+            var thead = table.AppendChild(htmlDocument.CreateElement("thead"));
+
+            var headerRow = thead.AppendChild(htmlDocument.CreateElement("tr"));
+            headerRow.AppendChild(ElementWithText("th", S["Product Name"]));
+            #endregion
+
+            #region Table body
+            var tbody = table.AppendChild(htmlDocument.CreateElement("tbody"));
+
+            var itemRows = filteredCatalogue.Items
+                .Select(catalogueItem =>
+                {
+                    var row = htmlDocument.CreateElement("tr");
+
+                    var productNameCell = row.AppendChild(htmlDocument.CreateElement("td"));
+
+                    if (catalogueItem.Uri != null)
+                    {
+                        var link = productNameCell.AppendChild(ElementWithText("a", ItemName(catalogueItem)));
+                        link.SetAttributeValue("href", catalogueItem.Uri.AbsoluteUri);
+                    }
+                    else
+                    {
+                        productNameCell.AppendChild(htmlDocument.CreateTextNode(ItemName(catalogueItem)));
+                    }
+
+                    return row;
+                });
+
+            foreach (var row in itemRows)
+            {
+                tbody.AppendChild(row);
+            }
+            #endregion
+            #endregion
+
+            using var writer = new StringWriter();
+
+            htmlDocument.Save(writer);
+
+            return writer.ToString();
+
+            HtmlNode ElementWithText(string elementName, string text)
+            {
+                var element = htmlDocument.CreateElement(elementName);
+                element.AppendChild(htmlDocument.CreateTextNode(text));
+
+                return element;
+            }
+        }
+
+        private string GetPlainTextContent(string summary, Catalogue filteredCatalogue)
+        {
+            var content = new StringBuilder(EmailTemplates.PlainTextTemplate);
+
+            content.Replace("%SUMMARY%", summary);
+
+            var list = string.Join(
+                Environment.NewLine,
+                filteredCatalogue.Items
+                    .Select(catalogueItem =>
+                    {
+                        var row = $"- {ItemName(catalogueItem)}";
+
+                        if (catalogueItem.Uri != null)
+                        {
+                            row += $" ({catalogueItem.Uri.AbsoluteUri})";
+                        }
+
+                        return row;
+                    })
+            );
+
+            content.Replace("%ITEMSLIST%", list);
+
+            return content.ToString();
+        }
+
+        private string ItemName(CatalogueItem catalogueItem) => catalogueItem.Name ?? S["Unknown Item"];
     }
 }
