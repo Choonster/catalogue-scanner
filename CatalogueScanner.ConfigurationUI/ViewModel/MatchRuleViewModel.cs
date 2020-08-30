aa377cb8104ffa6175a74d6b7a9d19603c906c2f
Choonster TheMage <Choonster.2010@gmail.com>
2020-08-31 01:50:13 +1000

    Improve Matching Options page

--- CatalogueScanner.ConfigurationUI/ViewModel/MatchRuleViewModel.cs
+++ CatalogueScanner.ConfigurationUI/ViewModel/MatchRuleViewModel.cs
@@ -1,12 +1,25 @@
 ﻿using CatalogueScanner.Core.Options;
+using Microsoft.AspNetCore.Components.Forms;
+using System.ComponentModel.DataAnnotations;
 
 namespace CatalogueScanner.ConfigurationUI.ViewModel
 {
     public class MatchRuleViewModel
     {
         public CatalogueItemProperty Property { get; set; }
+
         public MatchType MatchType { get; set; }
-        public string? Value { get; set; }
+
+        [Required]
+        public string Value { get; set; } = string.Empty;
+
         public bool InEditMode { get; set; }
+
+        public EditContext EditContext { get; }
+
+        public MatchRuleViewModel()
+        {
+            EditContext = new EditContext(this);
+        }
     }
 }
