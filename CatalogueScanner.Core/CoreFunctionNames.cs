6a29f13a10f7e98c671a373fdcf75142e1d24552
Choonster TheMage <Choonster.2010@gmail.com>
2021-02-06 02:32:08 +1100

    Fix doc comments in function name classes

--- CatalogueScanner.Core/CoreFunctionNames.cs
+++ CatalogueScanner.Core/CoreFunctionNames.cs
@@ -1,16 +1,14 @@
-﻿using CatalogueScanner.Core.Functions;
-
 namespace CatalogueScanner.Core
 {
     public static class CoreFunctionNames
     {
         /// <summary>
-        /// The name of the <see cref="FilterCatalogueItem.FilterCatalogueItem"/> activity function.
+        /// The name of the <see cref="Functions.FilterCatalogueItem"/> activity function.
         /// </summary>
         public const string FilterCatalogueItem = "FilterCatalogueItem";
 
         /// <summary>
-        /// The name of the <see cref="SendCatalogueDigestEmail.SendCatalogueDigestEmail"/> activity function.
+        /// The name of the <see cref="Functions.SendCatalogueDigestEmail"/> activity function.
         /// </summary>
         public const string SendCatalogueDigestEmail = "SendCatalogueDigestEmail";
     }
--- CatalogueScanner.SaleFinder/SaleFinderFunctionNames.cs
+++ CatalogueScanner.SaleFinder/SaleFinderFunctionNames.cs
@@ -1,33 +1,31 @@
-﻿using CatalogueScanner.SaleFinder.Functions;
-
-namespace CatalogueScanner.SaleFinder
+﻿namespace CatalogueScanner.SaleFinder
 {
     internal static class SaleFinderFunctionNames
     {
         /// <summary>
-        /// The name of the <see cref="CheckColesCatalogue.CheckColesCatalogue"/> function.
+        /// The name of the <see cref="Functions.CheckColesCatalogue"/> function.
         /// </summary>
         public const string CheckColesCatalogue = "CheckColesCatalogue";
 
         /// <summary>
-        /// The name of the <see cref="CheckWoolworthsCatalogue.CheckWoolworthsCatalogue"/> function.
+        /// The name of the <see cref="Functions.CheckWoolworthsCatalogue"/> function.
         /// </summary>
         public const string CheckWoolworthsCatalogue = "CheckWoolworthsCatalogue";
 
 
         /// <summary>
-        /// The name of the <see cref="ScanSaleFinderCatalogue.ScanSaleFinderCatalogue.ScanSaleFinderCatalogue"/> orchestrator function.
+        /// The name of the <see cref="Functions.ScanSaleFinderCatalogue"/> orchestrator function.
         /// </summary>
         public const string ScanSaleFinderCatalogue = "ScanSaleFinderCatalogue";
 
         /// <summary>
-        /// The name of the <see cref="ScanSaleFinderCatalogue.ScanSaleFinderCatalogue.ScanSaleFinderCatalogue"/> orchestration trigger function.
+        /// The name of the <see cref="Functions.ScanSaleFinderCatalogue"/> orchestration trigger function.
         /// </summary>
         public const string ScanSaleFinderCatalogue_QueueStart = "ScanSaleFinderCatalogue_QueueStart";
 
 
         /// <summary>
-        /// The name of the <see cref="DownloadSaleFinderCatalogue.DownloadSaleFinderCatalogue"/> activity function.
+        /// The name of the <see cref="Functions.DownloadSaleFinderCatalogue"/> activity function.
         /// </summary>
         public const string DownloadSaleFinderCatalogue = "DownloadSaleFinderCatalogue";
     }
