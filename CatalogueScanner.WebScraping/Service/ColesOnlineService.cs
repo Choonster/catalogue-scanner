ff08a8abe0366db5a67cf817eca18c9472945503
Choonster TheMage <Choonster.2010@gmail.com>
2021-08-15 08:17:18 +1000

    Initial attempt to get Web Scraping API authentication working

--- CatalogueScanner.WebScraping/Service/ColesOnlineService.cs
+++ CatalogueScanner.WebScraping/Service/ColesOnlineService.cs
@@ -1,8 +1,13 @@
 ﻿using CatalogueScanner.Core.Utility;
 using CatalogueScanner.WebScraping.Common.Dto.ColesOnline;
+using CatalogueScanner.WebScraping.Options;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
+using Microsoft.Identity.Web;
 using Newtonsoft.Json;
 using System;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace CatalogueScanner.WebScraping.Service
@@ -10,10 +15,24 @@ namespace CatalogueScanner.WebScraping.Service
     public class ColesOnlineService
     {
         private readonly HttpClient httpClient;
+        private readonly WebScrapingApiOptions webScrapingApiOptions;
+        private readonly ITokenAcquisition tokenAcquisition;
+        private readonly string tokenScope;
 
-        public ColesOnlineService(HttpClient httpClient)
+        public ColesOnlineService(HttpClient httpClient, IOptionsSnapshot<WebScrapingApiOptions> webScrapingApiOptions, ITokenAcquisition tokenAcquisition)
         {
-            this.httpClient = httpClient;
+            #region null checks
+            if (webScrapingApiOptions is null)
+            {
+                throw new ArgumentNullException(nameof(webScrapingApiOptions));
+            }
+            #endregion
+
+            this.httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
+            this.tokenAcquisition = tokenAcquisition ?? throw new ArgumentNullException(nameof(tokenAcquisition));
+            this.webScrapingApiOptions = webScrapingApiOptions.Value;
+
+            tokenScope = new Uri(this.webScrapingApiOptions.BaseAddress!, ".default").AbsoluteUri;
         }
 
         /// <summary>
@@ -32,7 +51,12 @@ namespace CatalogueScanner.WebScraping.Service
 
         private async Task<T> GetAsync<T>(string path)
         {
-            var response = await httpClient.GetAsync(path).ConfigureAwait(false);
+            var token = await tokenAcquisition.GetAccessTokenForAppAsync(tokenScope, authenticationScheme: Constants.Bearer).ConfigureAwait(false);
+
+            using var request = new HttpRequestMessage(HttpMethod.Get, new Uri(path, UriKind.Relative));
+            request.Headers.Authorization = new AuthenticationHeaderValue(JwtBearerDefaults.AuthenticationScheme, token);
+
+            var response = await httpClient.SendAsync(request).ConfigureAwait(false);
 
             response.EnsureSuccessStatusCode();
 
--- CatalogueScanner.WebScraping/WebScrapingCatalogueScannerPlugin.cs
+++ CatalogueScanner.WebScraping/WebScrapingCatalogueScannerPlugin.cs
@@ -3,8 +3,10 @@ using CatalogueScanner.ConfigurationUI.Extensions;
 using CatalogueScanner.Core.Host;
 using CatalogueScanner.WebScraping.Options;
 using CatalogueScanner.WebScraping.Service;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
+using Microsoft.Identity.Web;
 using System;
 
 namespace CatalogueScanner.WebScraping
@@ -41,6 +43,10 @@ namespace CatalogueScanner.WebScraping
                 var baseAddress = httpClient.BaseAddress ?? throw new InvalidOperationException($"{nameof(httpClient)}.{nameof(httpClient.BaseAddress)} is null");
                 httpClient.BaseAddress = httpClient.BaseAddress.AppendPath("coles-online/");
             });
+
+            builder.Services.AddMicrosoftIdentityWebApiAuthentication(builder.LocalConfiguration)
+                .EnableTokenAcquisitionToCallDownstreamApi(options => builder.LocalConfiguration.GetSection(Constants.AzureAd).Bind(options))
+                .AddInMemoryTokenCaches();
 
             AddConfiguration(builder);
         }
