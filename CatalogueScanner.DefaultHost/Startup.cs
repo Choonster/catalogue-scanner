8e15324811224064902d6d9b2c0b529a37e01b77
Choonster TheMage <Choonster.2010@gmail.com>
2021-11-20 18:25:15 +1100

    Include file permissions

--- CatalogueScanner.DefaultHost/Startup.cs
+++ CatalogueScanner.DefaultHost/Startup.cs
@@ -33,7 +33,7 @@ namespace CatalogueScanner.DefaultHost
                     RedirectStandardOutput = true,
                     RedirectStandardError = true,
                     CreateNoWindow = true,
-                    Arguments = $"-c \"ls .",
+                    Arguments = $"-c \"ls -l .",
                 },
             };
 
