7a2565daa60201d9b2b435c2fe812e3698d2a5e7
Choonster TheMage <Choonster.2010@gmail.com>
2021-12-21 04:13:23 +1100

    Log message when starting up

--- CatalogueScanner.DefaultHost/Startup.cs
+++ CatalogueScanner.DefaultHost/Startup.cs
@@ -39,6 +39,8 @@ namespace CatalogueScanner.DefaultHost
             });
 #pragma warning restore CA2000 // Dispose objects before losing scope
 
+            Console.Error.WriteLine("catalogue-scanner starting up!");
+
             // Install the browser required by Playwright 
             Microsoft.Playwright.Program.Main(new[] { "install chromium" });
 
