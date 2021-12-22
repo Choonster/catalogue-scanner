885895cb8608c6812bb752eb577fba7d7171a120
Choonster TheMage <Choonster.2010@gmail.com>
2021-12-23 01:31:32 +1100

    Remove now-unused Playwright publishing files

--- CatalogueScanner.DefaultHost/Publish/playwright_install.bat
+++
@@ -1,5 +0,0 @@
-set PLAYWRIGHT_BROWSERS_PATH=%APPSETTING_PLAYWRIGHT_BROWSERS_PATH%
-
-dotnet tool install --global Microsoft.Playwright.CLI
-
-"%USERPROFILE%\.dotnet\tools\playwright" -p "%~dp0\..\..\..\wwwroot" install chromium
