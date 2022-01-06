a7841f312717d7e0335a594974a5fb1adb2f635c
Choonster TheMage <Choonster.2010@gmail.com>
2022-01-07 07:08:49 +1100

    Partially fix deserialisation of SaleFinderCatalogue - Move Macross.Json.Extensions dependency to CatalogueScanner.Core - Add BaseJsonCollectionItemConverter to handle collections with custom item converters -- Not fully working yet - Add [JsonInclude] and internal set to collection/dictionary properties - Change PageConverter to use [JsonExtensionData] and separate SerializerContext

---
+++ CatalogueScanner.SaleFinder/Dto/SaleFinder/SaleFinderInternalSerializerContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CatalogueScanner.SaleFinder.Dto.SaleFinder
+{
+    [JsonSourceGenerationOptions(
+        WriteIndented = true,
+        PropertyNamingPolicy = JsonKnownNamingPolicy.CamelCase
+    )]
+    [JsonSerializable(typeof(PageWithExtensionData))]
+    [JsonSerializable(typeof(Shape))] // TODO: Remove when https://github.com/dotnet/runtime/pull/62643 is merged/released
+    internal partial class SaleFinderInternalSerializerContext : JsonSerializerContext
+    {
+    }
+}
