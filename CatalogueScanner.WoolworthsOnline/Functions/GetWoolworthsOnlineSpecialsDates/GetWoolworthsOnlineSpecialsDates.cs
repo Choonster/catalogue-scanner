ef7751a81ec60f6d5f8dd9b9eb5cc78cfd8338a3
Choonster TheMage <Choonster.2010@gmail.com>
2023-10-29 00:54:33 +1100

    Remove leftover null check

--- CatalogueScanner.WoolworthsOnline/Functions/GetWoolworthsOnlineSpecialsDates/GetWoolworthsOnlineSpecialsDates.cs
+++ CatalogueScanner.WoolworthsOnline/Functions/GetWoolworthsOnlineSpecialsDates/GetWoolworthsOnlineSpecialsDates.cs
@@ -10,13 +10,6 @@ namespace CatalogueScanner.WoolworthsOnline.Functions
         [Function(WoolworthsOnlineFunctionNames.GetWoolworthsOnlineSpecialsDates)]
         public static DateRange Run([ActivityTrigger] object? _)
         {
-            #region null checks
-            if (context is null)
-            {
-                throw new ArgumentNullException(nameof(context));
-            }
-            #endregion
-
             var specialsResetTime = WoolworthsOnlineService.SpecialsResetTime;
 
             var now = DateTimeOffset.UtcNow;
