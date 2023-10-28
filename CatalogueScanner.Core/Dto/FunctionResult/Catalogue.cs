27dd7e8ef7345c35bc74c2ab3f3e80c1a5fda776
Choonster TheMage <Choonster.2010@gmail.com>
2023-10-29 08:13:45 +1100

    Add JsonConverter for CultureInfo - Convert SaleFinderCatalogueDownloadInformation to a record

--- CatalogueScanner.Core/Dto/FunctionResult/Catalogue.cs
+++ CatalogueScanner.Core/Dto/FunctionResult/Catalogue.cs
@@ -1,8 +1,16 @@
-﻿using System;
+﻿using CatalogueScanner.Core.Serialisation;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
+using System.Text.Json.Serialization;
 
 namespace CatalogueScanner.Core.Dto.FunctionResult
 {
-    public record Catalogue(string Store, DateTimeOffset StartDate, DateTimeOffset EndDate, CultureInfo CurrencyCulture, ICollection<CatalogueItem> Items);
+    public record Catalogue(
+        string Store,
+        DateTimeOffset StartDate,
+        DateTimeOffset EndDate,
+        [property: JsonConverter(typeof(CultureInfoConverter))] CultureInfo CurrencyCulture,
+        ICollection<CatalogueItem> Items
+    );
 }
---
+++ CatalogueScanner.Core/Serialisation/CultureInfoConverter.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace CatalogueScanner.Core.Serialisation
+{
+    public class CultureInfoConverter : JsonConverter<CultureInfo>
+    {
+        public override CultureInfo? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            #region null checks
+            if (typeToConvert is null)
+            {
+                throw new ArgumentNullException(nameof(typeToConvert));
+            }
+
+            if (options is null)
+            {
+                throw new ArgumentNullException(nameof(options));
+            }
+            #endregion
+
+            if (reader.TokenType != JsonTokenType.String)
+            {
+                throw new JsonException($"Error reading {typeof(CultureInfo)} from {typeof(Utf8JsonReader)}. Current item is not a string: {reader.TokenType}");
+            }
+
+            var stringValue = reader.GetString();
+
+            if (stringValue == null)
+            {
+                return null;
+            }
+
+            try
+            {
+                return CultureInfo.GetCultureInfo(stringValue);
+            }
+            catch (CultureNotFoundException ex)
+            {
+                throw new JsonException($"Error reading {typeof(CultureInfo)} from {typeof(Utf8JsonReader)}. Culture not found.", ex);
+            }
+        }
+
+        public override void Write(Utf8JsonWriter writer, CultureInfo value, JsonSerializerOptions options)
+        {
+            #region null checks
+            if (writer is null)
+            {
+                throw new ArgumentNullException(nameof(writer));
+            }
+
+            if (value is null)
+            {
+                throw new ArgumentNullException(nameof(value));
+            }
+
+            if (options is null)
+            {
+                throw new ArgumentNullException(nameof(options));
+            }
+            #endregion
+
+            writer.WriteStringValue(value.Name);
+        }
+    }
+}
